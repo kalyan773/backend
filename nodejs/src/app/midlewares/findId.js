55449dccd1bb459f5f112c28c61537db25af87a1
kalyan773 <kalyanbandi773@gmail.com>
2025-08-07 00:23:08 +0530

    test

--- nodejs/src/app/midlewares/findId.js
+++ nodejs/src/app/midlewares/findId.js
@@ -18,6 +18,7 @@ let middleware = {
       // Fetch all employee IDs from the database
       let result = await findId(empId);
       console.log("Fetched IDs from DB:", result);
+      console.log("frghjkl")
 
       if (result.length === 0) {
         // If ID doesn't exist, respond with an error
